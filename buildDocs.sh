4c565b902d2bd7fe20ac8293ae1f7c3837375f2c
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-08-23 11:38:44 +0200

    do not exit at first error

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-set -e
 ################################################################################
 # File:    buildDocs.sh
 # Purpose: Script that builds our documentation using sphinx and updates GitHub
